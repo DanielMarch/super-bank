9ba821ed9129a996087dfce5a51d233c9d0e7303
Seth March <seth.d.march@gmail.com>
2014-04-15 09:40:29 -0500

    push to git

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -42,10 +42,10 @@
     var input = $('#amount').val() * 1;
     var output = balance - input * 1;
     if(balance < 0){
-      output = balance - input - 50;
+      output = balance - input - 50 * 1;
       $td1.text('(' + '$' + 50 + ')');
       $td3.text('(' + '$' + input + ')');
-      $td4.text('$' + output);
+      $td4.text('(' + '$' + output + ')');
       $tr.append($td1,$td2,$td3,$td4);
       $('#table').append($tr);
     }else{
@@ -55,7 +55,6 @@
       $('#table').append($tr);
     }
     $($td1).css('color', 'green');
-    $($td2).css('color', 'blue');
     $($td3).css('color', 'red');
     $($td4).css('color', 'black');
     update(output);
